5ec577ce6fac2c99f7a302cdc28bc8a485304a03
Willy Takang <wtakang@e-panzer.com>
2021-04-24 09:45:44 -0400

    added a function to exist the system

--- another_file.py
+++ another_file.py
@@ -6,3 +6,7 @@ import datetime
 def print_time():
     """This function prints the system time"""
     print(datetime.datetime.now())
+
+def exit_application():
+    """exits the system when there is a problem"""
+    sys.exit()
